d3f50cc69a63b9c1cf7597b08eb38191f28d20f3
Jianbin Zhang <jianbinzhang@rrc.ca>
2026-01-13 21:21:08 -0600

    Import componenets to app.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Department from "./components/Department";
+import { departments } from "./data/departments";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1>Pixell River Employee Directory</h1>
-      </div>
+      <Header />
+
+      <main>
+        {departments.map((dept, index) => (
+          <Department key={index} dept={dept} />
+        ))}
+      </main>
+
+      <Footer />
+
     </>
   )
 }
